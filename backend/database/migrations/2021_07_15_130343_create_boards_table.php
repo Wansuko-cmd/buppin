6f660fd79ebe2b063d1f98dfa952797d440fe0ed
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-07-15 22:12:10 +0900

    [add] migrationfファイルを追加

---
+++ backend/database/migrations/2021_07_15_130343_create_boards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->comment('物品名');
+            $table->string('label_number', 50)->nullable()->comment('テプラナンバー');
+            $table->boolean('is_personal')->comment('私物かどうか');
+            $table->string('photo_path')->nullable()->comment('写真のURL');
+            $table->string('detail', 200)->comment('紹介文');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
